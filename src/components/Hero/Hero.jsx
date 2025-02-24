fa76310f36f7d2a3fffdb9c1624f2b37691bed10
SSworup77 <98733444+SSworup77@users.noreply.github.com>
2025-02-24 22:19:54 +0545

    Made website responsive u/2

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,9 +7,7 @@ const Hero = () => {
       <div className="hero-text">
         <h1>We ensure better education for a better world</h1>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui saepe quam aut, 
-          architecto minus nemo repellat laudantium reiciendis. Eius recusandae repellendus enim nam
-           deserunt inventore voluptatibus suscipit fugit totam quam.</p>
-
+          architecto minus nemo repellat laudantium reiciendis. Eius recusandae repellendus enim nam</p>
         <button className='btn'>Learn More <img src={dark_arrow} alt="" /></button>
 
       </div>
