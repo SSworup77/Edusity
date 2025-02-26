98b0b52ac522220da26d8b5f4b71761ca1182d89
SSworup77 <98733444+SSworup77@users.noreply.github.com>
2025-02-26 21:14:05 +0545

    Added engaging features

--- src/components/Campus/Campus.jsx
+++ src/components/Campus/Campus.jsx
@@ -10,10 +10,10 @@ const Campus = () => {
   return (
     <div className='campus'>
         <div className="gallery">
-            <img src={gallery_1} alt="" />
-            <img src={gallery_2} alt="" />
-            <img src={gallery_3} alt="" />
-            <img src={gallery_4} alt="" />
+            <div className="image-wrap"><img src={gallery_1} alt="" /></div>
+            <div className="image-wrap"><img src={gallery_2} alt="" /></div>
+            <div className="image-wrap"><img src={gallery_3} alt="" /></div>
+            <div className="image-wrap"><img src={gallery_4} alt="" /></div>
         </div>
         <button className='btn dark-btn'>See more here <img src={white_arrow} alt="" /></button>
     </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
 import { Link, Element } from 'react-scroll';
@@ -6,19 +6,35 @@ import menu_icon from '../../assets/menu-icon.png'
 
 const Navbar = () => {
   const [sticky,setSticky]=useState(false)
+  const [mobileMenu,setMobileMenu]=useState(false)
+  const menuRef=useRef(null)
   useEffect(()=>{
     window.addEventListener('scroll',()=>{
       window.scrollY>150 ? setSticky(true) : setSticky(false)
     })
   },[]);
-  const [mobileMenu,setMobileMenu]=useState(false)
+  useEffect(()=>{
+    const handleClickOutside=(e)=>{
+      if(menuRef.current && !menuRef.current.contains(e.target)){
+        setMobileMenu(false);
+      }
+    };
+    if(mobileMenu){
+      document.addEventListener('touchstart',handleClickOutside)
+    }
+    else{
+      document.removeEventListener('touchstart',handleClickOutside)
+    }
+    return ()=> document.removeEventListener('touchstart',handleClickOutside)
+  },[mobileMenu]);
+
   const toggleMenu = ()=>{
       mobileMenu?setMobileMenu(false):setMobileMenu(true);
   }
   return (
     <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
         <img src={logo} alt="" className='logo'/>
-        <ul className={mobileMenu?'':'hide-mobile-menu'}>
+        <ul className={mobileMenu?'':'hide-mobile-menu'} ref={menuRef}>
             <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
             <li><Link to='programs' smooth={true} offset={-260} duration={500}>Program</Link></li>
             <li><Link to='about' smooth={true} offset={-150} duration={500}>About Us</Link></li>
